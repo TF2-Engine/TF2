b06600f002eb6f403c0cc12fe006d5e8105cac49
TF2 <tf2@inspur.com>
2019-12-27 19:42:28 +0800

    resnet50_pruned.h is revised.

--- Runtime_Engine/cnn/device/full_compile.sh
+++ Runtime_Engine/cnn/device/full_compile.sh
@@ -1,4 +1,4 @@
 #nohup aoc -v --report src/cnn.cl &
 #nohup aoc -v --report -DRESNET50 src/cnn.cl &
-nohup aoc -v --report -DRESNET50 src/cnn.cl &
+nohup aoc -v --report -DRESNET50_PRUNED src/cnn.cl &
 #nohup aoc -v -c --report -DRESNET50 src/cnn.cl &
--- Runtime_Engine/cnn/recompile.sh
+++ Runtime_Engine/cnn/recompile.sh
@@ -1,3 +1,3 @@
 #aoc -march=emulator device/src/cnn.cl -o device/googlenet/cnn.aocx -v
-aoc -march=emulator -DRESNET50 device/src/cnn.cl -o device/resnet50/cnn.aocx -v
-#aoc -march=emulator -DRESNET50_PRUNED device/src/cnn.cl -o device/resnet50_pruned/cnn.aocx -v
+#aoc -march=emulator -DRESNET50 device/src/cnn.cl -o device/resnet50/cnn.aocx -v
+aoc -march=emulator -DRESNET50_PRUNED device/src/cnn.cl -o device/resnet50_pruned/cnn.aocx -v
